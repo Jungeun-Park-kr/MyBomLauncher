099edfb61914601fb354a1d2ddb54f42d49dd986
pje8845 <je991025@gmail.com>
2021-02-18 15:21:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/customlauncher/AppsDrawer.java
@@ -0,0 +1,14 @@
+package com.example.customlauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppsDrawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_drawer);
+    }
+}
---
+++ app/src/main/java/com/example/customlauncher/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.customlauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView chromeIcon = (ImageView)findViewById(R.id.chromeButton);
+        chromeIcon.setImageDrawable(getActivityIcon(this,"com.android.chrome", "com.google.android.apps.chrome.Main"));
+    }
+
+    public Drawable getActivityIcon(Context context, String packageName, String activityName) {
+        PackageManager pm = context.getPackageManager();
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(packageName, activityName));
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, 0);
+        return resolveInfo.loadIcon(pm);
+    }
+
+    public void onChromeButtonClick(View v) {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.chrome");
+        startActivity(launchIntent);
+    }
+}
